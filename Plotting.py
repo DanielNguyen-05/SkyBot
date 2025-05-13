6b218447870862e54442fcbe57ce0fbe8be0932b
KhoiHai <162028733+KhoiHai@users.noreply.github.com>
2025-05-14 00:59:55 +0700

    Update Plotting.py

--- Plotting.py
+++ Plotting.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import mplfinance as mpf
 
 def plot_candlestick(data: pd.DataFrame, title: str = "Candlestick Chart", ylabel: str = "Giá", volume: bool = True):
@@ -12,3 +13,18 @@ def plot_candlestick_from_csv(csv_path: str, title: str = "Candlestick Chart", y
         plot_candlestick(df, title=title, ylabel=ylabel, volume=volume)
     except Exception as e:
         print(f"Error loading CSV or plotting candlestick: {e}")
+
+def plot_forecast(forecast: pd.DataFrame):
+    plt.figure(figsize=(10, 6))
+
+    plt.plot(forecast['ds'], forecast['yhat'], label='Dự báo (yhat)', color='blue')
+    plt.fill_between(forecast['ds'], forecast['yhat_lower'], forecast['yhat_upper'], color='gray', alpha=0.2, label='Khoảng tin cậy')
+
+    plt.title('Dự báo giá (Forecast)')
+    plt.xlabel('Ngày')
+    plt.ylabel('Giá đóng cửa')
+
+    plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
