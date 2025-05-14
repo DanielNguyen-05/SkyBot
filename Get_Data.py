4fb226e2e75d1bc48c3f0a53fce970139c40171a
DanielNguyen-05 <nhdang231@clc.fitus.edu.vn>
2025-05-14 09:29:26 +0700

    update the SkyBot for identifying, analysing and responding about investment

--- Get_Data.py
+++ Get_Data.py
@@ -2,17 +2,17 @@ from alpaca_trade_api import REST
 import pandas as pd
 from datetime import datetime
 
-def get_data(api_key: str, api_secret: str, symbol: str, timeframe: str = "1Month", start: str = "2024-01-01"):
+def get_data(alpaca_api_key: str, alpaca_api_secret: str, symbol: str, timeframe: str = "1Month", start: str = "2024-01-01"):
     # Tạo kết nối với Alpaca API
     base_url = "https://paper-api.alpaca.markets"
-    api = REST(api_key, api_secret, base_url=base_url)
+    alpaca_api = REST(alpaca_api_key, alpaca_api_secret, base_url=base_url)
 
     # Lấy dữ liệu và lưu vào dataframe
-    bars = api.get_bars(symbol, timeframe=timeframe, start=start).df
+    bars = alpaca_api.get_bars(symbol, timeframe=timeframe, start=start).df
     return bars
 
 def save_to_csv(dataframe: pd.DataFrame, symbol: str):
     # Lưu dữ liệu vào file CSV
     file_path = f"Data/{symbol}_du_lieu.csv"
     dataframe.to_csv(file_path, index=False)
-    print(f"Dữ liệu đã được lưu vào {file_path}")
+    # print(f"Dữ liệu đã được lưu vào {file_path}")
--- Get_News.py
+++ Get_News.py
@@ -2,11 +2,11 @@ from alpaca_trade_api import REST
 from datetime import datetime, timedelta
 from typing import List
 
-def get_news(api_key: str, api_secret: str, symbol: str, base_url: str = "https://paper-api.alpaca.markets", days_ago: int = 3) -> List[str]:
-    api = REST(api_key, api_secret, base_url=base_url)
+def get_news(alpaca_api_key: str, alpaca_api_secret: str, symbol: str, base_url: str = "https://paper-api.alpaca.markets", days_ago: int = 3) -> List[str]:
+    alpaca_api = REST(alpaca_api_key, alpaca_api_secret, base_url=base_url)
     end_date = datetime.now()
     start_date = end_date - timedelta(days=days_ago)
     
-    news = api.get_news(symbol=symbol, start=start_date.strftime("%Y-%m-%d"), end=end_date.strftime("%Y-%m-%d"))
+    news = alpaca_api.get_news(symbol=symbol, start=start_date.strftime("%Y-%m-%d"), end=end_date.strftime("%Y-%m-%d"))
     headlines = [article.__dict__["_raw"]["headline"] for article in news]
     return headlines
